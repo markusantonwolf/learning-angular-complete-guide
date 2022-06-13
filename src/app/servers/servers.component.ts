65eb4d86d676d37b6bddf2ac63d307655887a69b
Markus Anton Wolf <wolf.markus.anton@gmail.com>
2022-06-13 08:15:46 +0200

    Changes

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,7 +5,24 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './servers.component.html',
 })
 export class ServersComponent implements OnInit {
+    new_server: string = '';
+    servers: string[] = [
+        'Test 1234',
+        'Hallo welt'
+    ];
+
     constructor() {}
 
     ngOnInit(): void {}
+
+    addServer() {
+        this.servers.push(this.new_server);
+        this.new_server = '';
+    }
+
+    removeServer(server: string) {
+        this.servers = this.servers.filter((item) => {
+            return item != server;
+        });
+    }
 }
